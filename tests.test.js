f774dcb4012c4a32e33122ac3b64e7e371fce916
melashu <meshu102@outlook.com>
2022-10-06 10:39:53 +0300

    add test

--- tests.test.js
+++ tests.test.js
@@ -4,3 +4,10 @@ test("Test balaced sum", () => {
   const actualValue = balancedSums([1, -2, 5, 6, 3, 4, 0, 0, -9, -10]);
   expect([0.2.toFixed(6),0.5.toFixed(6),0.3.toFixed(6)]).toEqual(actualValue);
 });
+
+test("Test balaced sum", () => {
+  const actualValue = balancedSums([1, -2, 5, 6, 3, 4, 0, 0, -9, -10]);
+  expect([(0.2).toFixed(6), (0.5).toFixed(6), (0.3).toFixed(6)]).toEqual(
+    actualValue
+  );
+});
